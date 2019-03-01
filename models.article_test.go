4c1b26000d26040a49b91a805b0e7b51acf103c8
Mohamed Ahmed <motawfik1998@gmail.com>
2019-03-01 22:20:39 +0200

    Added article struct and fictional array with the test file for the function

---
+++ models.article_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestGetAllArticles(t *Testing.T) {
+  alist := getAllArticles()
+
+  if len(alist) != len(articleList) {
+    t.Fail()
+  }
+
+  for i, v := range alist {
+    if v.Content != articleList[i].Content ||
+       v.ID != articleList[i].ID ||
+       v.Title != articleList[i].Title {
+         t.Fail()
+         break
+       }
+  }
+}
