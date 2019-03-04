b52f27bd220bba593f607934651877b89ea08a17
Mohamed Ahmed <motawfik1998@gmail.com>
2019-03-04 17:13:05 +0200

    Viewing a specific article

--- handlers.article.go
+++ handlers.article.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "net/http"
+  "strconv"
   "github.com/gin-gonic/gin"
 )
 
@@ -20,4 +21,31 @@ func showIndexPage(c *gin.Context) {
       "payload": articles,
     },
   )
+}
+
+func getArticle(c *gin.Context) {
+  // check if the article ID is valid
+  if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+    // check if the article exists
+    if article, err := getArticleByID(articleID); err == nil {
+      // Call the HTML method of the Context to render a template
+      c.HTML (
+        // set the status code to OK
+        http.StatusOK,
+        // use the article.html
+        "article.html",
+        // passes the data that the pages use
+        gin.H {
+          "title": article.Title,
+          "payload": article,
+        },
+      )
+    } else {
+      // If the article is not found, abort with an error
+      c.AbortWithError(http.StatusNotFound, err)
+    }
+  } else {
+    // If an invalid article ID is specified in the URL, abort with an error
+    c.AbortWithStatus(http.StatusNotFound)
+  }
 }
--- routes.go
+++ routes.go
@@ -3,4 +3,6 @@ package main
 func initializeRoutes() {
   // handle the index route
   router.GET("/", showIndexPage)
+
+  router.GET("/article/view/:article_id", getArticle)
 }
