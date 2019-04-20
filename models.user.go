7478d793372e1324599bb358d4bcb81cef4e55ed
Mohamed Ahmed <motawfik1998@gmail.com>
2019-04-20 17:46:10 +0200

    Added the logic for logging in and out

--- models.user.go
+++ models.user.go
@@ -36,3 +36,12 @@ func isUsernameAvailable(username string) bool {
   }
   return true
 }
+
+func isUserValid(username, password string) bool {
+  for _, u := range userList {
+    if u.Username == username && u.Password == password {
+      return true
+    }
+  }
+  return false
+}
--- routes.go
+++ routes.go
@@ -9,9 +9,12 @@ func initializeRoutes() {
   userRoutes := router.Group("/u")
   {
     userRoutes.GET("/register", showRegistrationPage)
-
     userRoutes.POST("/register", register)
+
+    userRoutes.GET("/login", showLoginPage)
+		userRoutes.POST("/login", performLogin)
+		userRoutes.GET("/logout", logout)
   }
 
-  
+
 }
